a08aca2360395bc8af8004cb7a00041ac22319b6
Kiryl Drozhzha <109621519+Kirofeed@users.noreply.github.com>
2024-09-05 17:17:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -32,10 +32,10 @@ def rgb_to_hsl(r, g, b):
 def choose_color():
     color_code = colorchooser.askcolor(title="Choose color")[1]
     if color_code:
-        rgb_value = hex_to_rgb(color_code)
-        hsl_value = rgb_to_hsl(*rgb_value)
-        rgb_label.config(text=f"RGB: {rgb_value}")
-        hsl_label.config(text=f"HSL: {hsl_value}")
+        CMYK_value = hex_to_rgb(color_code)
+        LAB_value = rgb_to_hsl(*CMYK_value)
+        CMYK_label.config(text=f"RGB: {CMYK_value}")
+        LAB_label.config(text=f"HSL: {LAB_value}")
 
 # Создание окна
 root = tk.Tk()
@@ -43,18 +43,31 @@ root.minsize(500, 500)
 root.maxsize(1000, 600)
 root.title("Color Converter")
 
+# Настройка сетки для центрирования
+root.grid_columnconfigure(0, weight=1)
+root.grid_columnconfigure(1, weight=1)
+root.grid_rowconfigure(0, weight=1)
+root.grid_rowconfigure(1, weight=1)
+root.grid_rowconfigure(2, weight=1)
+
 # Кнопка для выбора цвета
 color_button = tk.Button(root, text="Choose Color", command=choose_color)
-color_button.columnconfigure(0, weight=1)
-color_button.rowconfigure(0, weight=1)
-color_button.grid(column=0, row=0)
+color_button.grid(row=0, column=0, columnspan=2, padx=5, pady=5)
 
-# Метки для отображения результатов
-rgb_label = tk.Label(root, text="RGB:")
-rgb_label.grid(row=1, column=0, pady=5)
-scale_button = ttk.Scale(root)
-scale_button.grid(row=1, column=1, padx=50, pady=5)
-hsl_label = tk.Label(root, text="HSL:")
-hsl_label.grid(row=2, column=0, pady=5)
+# Метка для CMYK
+CMYK_label = tk.Label(root, text="CMYK:")
+CMYK_label.grid(row=1, column=0, pady=5, sticky='e')
+
+# Ползунок для CMYK
+scale_button_CMYK = ttk.Scale(root)
+scale_button_CMYK.grid(row=1, column=1, padx=50, pady=5, sticky='w')
+
+# Метка для LAB
+LAB_label = tk.Label(root, text="LAB:")
+LAB_label.grid(row=2, column=0, pady=5, sticky='e')
+
+# Ползунок для LAB
+scale_button_LAB = ttk.Scale(root)
+scale_button_LAB.grid(row=2, column=1, padx=50, pady=5, sticky='w')
 
 root.mainloop()
